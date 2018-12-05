99b6e7d0cde6da5382efd6a047624794ec114098
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-05 12:48:58 -0800

    Finish maxChar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0
+  let maxChar = ''
+  let result = {};
+  for ( let char of str) {
+    if (!result[char]) {
+      result[char] = 1
+    } else {
+      result[char] += 1
+    }
+  }
+
+  for ( let char in result ) {
+    if ( result[char] > max ) {
+      max = result[char];
+      maxChar = char;
+    }
+  }
+  return maxChar
+}
 
 module.exports = maxChar;
