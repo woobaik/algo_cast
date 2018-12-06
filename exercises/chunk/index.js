fbfc33b738f58aee5965c43ccb7258cb7d1f514e
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-05 16:02:54 -0800

    Finish chunk with 2 method, slice, last element

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   const result = [];
+//   let index = 0;
+//
+//   while (index < array.length) {
+//     result.push(array.slice(index, index + size));
+//     index += size
+//   }
+//   return result
+//
+// }
+
+
+function chunk(array, size) {
+  const result = [];
+
+  for ( let ele of array ) {
+    const last = result[result.length - 1]
+
+    if ( !last || last.length === size) {
+      result.push([ele])
+    } else {
+      last.push(ele)
+    }
+  }
+  return result
+}
+
+
+
 
 module.exports = chunk;
