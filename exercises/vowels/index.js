44a22b1e0faac8c135b2ee712b051b2009ae1626
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-07 01:12:15 -0800

    finish vowels with regex

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  let matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0
+}
+
 
 module.exports = vowels;
+
+
+// function vowels(str) {
+//   let counter = 0;
+//   const vowels = ['a','e','i','o','u'];
+//
+//   for ( let char of str.toLowerCase() ) {
+//     if (vowels.includes(char)) {
+//       counter += 1
+//     }
+//   }
+//   return counter
+//
+// }
