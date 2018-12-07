bca261d8bbd280f15462fa594666025a422693bc
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-06 23:55:01 -0800

    Finish pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,22 +14,50 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const size = 2 * n - 1
-  const mid = Math.floor(size / 2)
-
-  for (let row = 0; row < n ; row++) {
-    let level = ''
-
-    for ( let col = 0; col < size; col++) {
-      if ( mid - row <= col && mid + row >= col) {
-        level += '#'
-      } else {
-        level += ' '
-      }
-    }
-    console.log(level)
+function pyramid(n, row = 0, line = '') {
+  if (row === n) {
+    return;
   }
+
+  if (line.length === 2 * n - 1) {
+    console.log(line);
+    return pyramid(n, row + 1);
+  }
+
+  const mid = Math.floor( (2 * n - 1) / 2)
+  let add;
+
+  if (mid - row <= line.length && mid + row >= line.legnth) {
+    add = '#';
+  } else {
+    add = ' ';
+  }
+  pyramid(n, row, line + add);
+
+
+
+
+
+  // pyramid(n, row + 1)
 }
 
 module.exports = pyramid;
+
+
+// function pyramid(n) {
+//   const size = 2 * n - 1
+//   const mid = Math.floor(size / 2)
+//
+//   for (let row = 0; row < n ; row++) {
+//     let level = ''
+//
+//     for ( let col = 0; col < size; col++) {
+//       if ( mid - row <= col && mid + row >= col) {
+//         level += '#'
+//       } else {
+//         level += ' '
+//       }
+//     }
+//     console.log(level)
+//   }
+// }
