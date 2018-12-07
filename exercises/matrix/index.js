c65c3dd0ce1bf63d0bdfe057d0304644cb2ae618
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-07 13:34:44 -0800

    Finish snake matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,129 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const result = [];
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  counter = 1;
+
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+
+  while (startCol <= endCol && startRow <= endRow) {
+
+    for (let i = startCol; i <= endCol; i++) {
+      result[startRow][i] = counter;
+      counter += 1;
+    }
+
+    startRow += 1;
+
+    for ( let i = startRow; i <= endRow; i++) {
+      result[i][endCol] = counter;
+      counter += 1;
+    }
+
+    endCol -= 1;
+
+    for (let i = endCol; i >= startCol; i--) {
+      result[endRow][i] = counter;
+      counter += 1;
+    }
+
+    endRow -= 1;
+
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startCol] = counter;
+      counter += 1;
+    }
+
+    startCol += 1;
+  }
+  console.log(result)
+  return result;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 
 module.exports = matrix;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function matrix(n) {
+//   const result = []
+//   let counter = 1;
+//   let startRow = 0;
+//   let endRow = n-1;
+//   let startCol = 0;
+//   let endCol = n - 1;
+//
+//   for (let i = 0; i < n; i++) {
+//     result.push([])
+//   }
+//
+//   while ( startRow <= endRow && startCol <= endCol) {
+//
+//     for ( let i = startCol; i <= endCol; i++) {
+//       result[startRow][i] = counter;
+//       counter += 1;
+//     }
+//     startRow += 1
+//
+//     for (let i = startRow; i <= endRow; i++) {
+//       result[i][endCol] = counter;
+//       counter += 1;
+//     }
+//
+//     endCol -= 1;
+//
+//     for (let i = endCol; i >= startCol; i--) {
+//       result[endRow][i] = counter;
+//       counter += 1;
+//     }
+//
+//     endRow -= 1;
+//
+//     for (let i = endRow; i >= startRow; i--) {
+//       result[i][startCol] = counter;
+//       counter += 1;
+//     }
+//
+//     startCol += 1;
+//   }
+//   return result;
+// }
