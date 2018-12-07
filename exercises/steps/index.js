f7f3386bcba67c0232e1f394b49516636d3eaf2f
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-06 20:43:44 -0800

    Finish steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,44 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   let progressArray = spaceGenerator(n).split('')
+//   for (let i = 0; i < n; i++) {
+//     progressArray[i] = '#'
+//     console.log(progressArray.join(''));
+//   }
+//
+// }
+//
+// function spaceGenerator(n) {
+//
+//   let space = '';
+//
+//   for (let i = 0; i < n; i++) {
+//     space += ' '
+//   }
+//   return space
+// }
+
+
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let line = '';
+
+    for (let col = 0; col < n; col++) {
+      if (col <= row) {
+        line += '#'
+      } else {
+        line += ' '
+      }
+
+    }
+    console.log(line);
+  }
+
+}
+
+
+
 
 module.exports = steps;
