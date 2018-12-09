e582603ef79f33cd36b7d930c78b96baf1194858
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-08 18:00:45 -0800

    Finish fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,29 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function memoization(fn) {
+  const cache = {}
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args]
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
+}
+
+
+function fib(n) {
+  if (n < 2) {
+    return n
+  }
+
+  return fib(n-1) + fib(n-2)
+}
+
+fib = memoization(fib)
+
 
 module.exports = fib;
