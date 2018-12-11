000b6698bfbbd0983b1c051f7c19cdcc0bc494e1
Joungwoo Baik <joungwoo.baik@gmail.com>
2018-12-10 23:52:24 -0800

    Finish linear gradient page

--- gradient/script.js
+++ gradient/script.js
@@ -1,1 +1,13 @@
 
+let content = document.querySelector('.content')
+let color1 = document.querySelector('#color-1')
+let color2 = document.querySelector('#color-2')
+// body.style.backgroundColor = '#D8BFD8'
+let colorChanger = () => content.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`
+
+color1.addEventListener('input',() => {
+  colorChanger()
+})
+color2.addEventListener('input',() => {
+  colorChanger()
+})
